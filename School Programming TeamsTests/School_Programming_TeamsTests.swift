c9749f4f5079af9099520c91721d1d8cc2b08782
Jupally,Hari Priya <s533990@ol10019m.nwmissouri.edu>
2019-03-13 20:32:10 -0500

    Initial Commit

---
+++ School Programming TeamsTests/School_Programming_TeamsTests.swift
@@ -0,0 +1,34 @@
+//
+//  School_Programming_TeamsTests.swift
+//  School Programming TeamsTests
+//
+//  Created by Jupally,Hari Priya on 3/13/19.
+//  Copyright © 2019 Jupally,Hari Priya. All rights reserved.
+//
+
+import XCTest
+@testable import School_Programming_Teams
+
+class School_Programming_TeamsTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
